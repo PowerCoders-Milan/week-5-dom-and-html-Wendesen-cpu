b4a01c7e261f8be37fb51bdba1ea52258e005662
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-03-02 10:55:41 +0100

    Done

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,5 +1,11 @@
 // Use this function that is linked to the button to change the background
 
 function set_background() {
-
+    let a = document.getElementsByTagName("p")[0];
+    let a = document.getElementsByTagName("p")[1];
+    let a = document.getElementsByTagName("p")[2];
+    a.style.backgroundColor = "green";
+    b.style.backgroundColor = "blue";
+    c.style.backgroundColor = "red";
+    
 }
