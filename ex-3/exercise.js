4ffd3d00966f893d9db97788d647a057a4b84dfc
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-03-02 14:10:45 +0100

    Done

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,11 @@
 function insert_Row() {
+    let theTable = document.getElementById("sampleTable");
+    let newrow = document.createElement("tr");
+    let cell1 = newrow.insertCell(0);
+    cell1.innerHTML="New Cell1"
+    let cell2 = newrow.insertCell(1);
+    cell2.innerHTML="New Cell1"
+    theTable.appendChild(newrow);
+
 
 }
