f407e7648a65fdc013dc24d52510d8d29346e656
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-03-02 15:55:13 +0100

    Done

--- ex-5/exercise.js
+++ ex-5/exercise.js
@@ -1,9 +1,34 @@
-function loadBooks(array) {
+
+
+
+
+function loadBooks(books) {
+    
+      books.forEach(function(item){
+
+        var a = document.createElement("p");
+        var b = document.createTextNode(item.title);
+        a.appendChild(b);
+
+        var c = document.createElement("p");
+        var d = document.createTextNode(item.author);
+        c.appendChild(d);
+
+        
+         document.getElementsByTagName("body")[0].appendChild(a);
+         document.getElementsByTagName("body")[0].appendChild(c);
+        // document.createElement("p").appendChild(document.createTextNode(item.title));
+        // document.createElement("p").appendChild(document.createTextNode(item.author));
+
+    
+      })
 
 }
 
 // Paste here your array
-
+var books = [
+    { title: 'The Design of EveryDay Things',  author: 'Don Norman', alreadyRead: false }, { title: 'The power of now', author: 'Ekhart Tolle', alreadyRead: true }, { title: 'The Three Body Problem', author: 'Liu Cixin', alreadyRead: true }
+  ];
 
 // This function will execute when the window loads
-window.onload = loadBooks(array);
+window.onload = loadBooks(books);
